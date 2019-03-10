c8af75f3c131160be2647698770e58e8b045bba0
SamEga <egorstepanow@mail.ru>
2019-03-10 18:23:14 +0700

    Add style phone input

--- js/main.js
+++ js/main.js
@@ -1,27 +1,7 @@
-popupPhoneHide();
-popupCameraHide();
-
-// Phone Mask
+// // Phone Mask
 $(document).ready(function() {
-  $('#phone').mask('+7 999 999 9999');
+  $('#phone').mask('+7 999 999 9999', { autoclear: false });
 });
-
-// Popup Functions
-function popupCameraHide() {
-  $('.popup-camera-wrapper').hide();
-}
-
-function popupCameraShow() {
-  $('.popup-camera-wrapper').show();
-}
-
-function popupPhoneHide() {
-  $('.popup-phone-wrapper').hide();
-}
-
-function popupPhoneShow() {
-  $('.popup-phone-wrapper').show();
-}
 
 // Change form with success input
 function submitForm() {
@@ -33,30 +13,71 @@ function submitForm() {
   newText.innerText = 'Спасибо, что оставили заявку. Мы скоро вам перезвоним.';
   const parent = document.querySelector('.phone-box-form');
   newText.classList.add('text-submited-form');
-
   const btnSubmit = document.querySelector('.btn-submit-form');
   btnSubmit.style.background = '#DAE0E5';
   btnSubmit.style.boxShadow = 'none';
-
   parent.append(newText);
 }
 
-// Listeners
-$('#popup-camera').click(function(e) {
-  e.stopPropagation();
+const phone = document.querySelector('#phone');
+const formInput = document.querySelector('#form');
+const numberNotify = document.querySelector('.number-notify');
+
+// Style Phone Input
+function invlaidInput() {
+  phone.classList.toggle('invalid');
+  numberNotify.textContent = 'Проверьте набранный номер';
+  setTimeout(function() {
+    phone.classList.toggle('invalid');
+    numberNotify.textContent = 'Контактный телефон';
+  }, 1000);
+}
+
+//Validate Phone Input
+formInput.addEventListener('submit', function(e) {
+  const contain = phone.value.indexOf('_') !== -1;
+  if (!contain) {
+    submitForm();
+  } else {
+    e.preventDefault();
+    invlaidInput();
+  }
 });
 
-$('#popup-phone').click(function(e) {
+// Camera Popup
+const cameraBox = document.querySelector('#camera-box');
+const popupCamera = document.querySelector('#popup-camera');
+const popupCameraWrapper = document.querySelector('.popup-camera-wrapper');
+const cameraPopupBtn = document.querySelector('.camera-popup-btn');
+
+cameraBox.addEventListener('click', function() {
+  popupCameraWrapper.classList.add('show-popup');
+});
+popupCameraWrapper.addEventListener('click', function() {
+  popupCameraWrapper.classList.remove('show-popup');
+});
+popupCamera.addEventListener('click', function(e) {
   e.stopPropagation();
 });
+cameraPopupBtn.addEventListener('click', function() {
+  popupCameraWrapper.classList.remove('show-popup');
+});
 
-$('#form').submit(submitForm);
+// Phone Popup
+const phoneBox = document.querySelector('#phone-box');
+const popupPhone = document.querySelector('#popup-phone');
+const popupPhoneWrapper = document.querySelector('.popup-phone-wrapper');
+const phonePopupBtn = document.querySelector('.phone-popup-btn');
 
-$('.camera-popup-btn').click(popupCameraHide);
-$('.phone-popup-btn').click(popupPhoneHide);
-
-$('#camera-box').click(popupCameraShow);
-$('.popup-camera-wrapper').click(popupCameraHide);
-
-$('#phone-box').click(popupPhoneShow);
-$('.popup-phone-wrapper').click(popupPhoneHide);
+phoneBox.addEventListener('click', function() {
+  popupPhoneWrapper.classList.add('show-popup');
+});
+popupPhoneWrapper.addEventListener('click', function() {
+  popupPhoneWrapper.classList.remove('show-popup');
+});
+popupPhone.addEventListener('click', function(e) {
+  e.stopPropagation();
+});
+phonePopupBtn.addEventListener('click', function() {
+  popupPhoneWrapper.classList.remove('show-popup');
+});
